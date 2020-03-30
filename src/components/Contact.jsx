e1835e9f4227082c49021b8b80553ac5e6c9078c
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-30 20:14:16 +0200

    props change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import './Contact.css';
 
-const name = 'Bonnie Day';
-const avatar = "https://randomuser.me/api/portraits/women/21.jpg";
-const Online= false;
 
-function Contact() { 
+function Contact (props) {
     return (
-        <div className="Contact">
-            <img src={avatar} className='avatar'/>
-            <div>
-                <h4 className="name">{name}</h4>
-                <div className='status'>
-                    <img className={Online ? 'status-online' : 'status-offline'}
-                    />
-                    <p className='status-text'>{Online ? 'Online' : 'Offline'} </p>
-                </div>
-            </div>
+      <div className='Contact'>
+        <img className='avatar' src={props.avatar}
+        />
+        <div>
+          <h4 className='name'>{props.name}
+          </h4>
+          <div className='status'>
+            <img className={props.online ? 'status-online' : 'status-offline'}
+            />
+            <p className='status-text'>{props.online ? 'online' : 'offline'}</p>
+          </div>
         </div>
-)
-}
+      </div>
+    );
+  }
 export default Contact;
