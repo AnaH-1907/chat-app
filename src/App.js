1b0e019ad6519282497d4c4ffc1ad5c00b623ea2
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-30 20:13:08 +0200

    props change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact 
+      name = 'Bonnie Day'
+      avatar = "https://randomuser.me/api/portraits/women/21.jpg"
+      online = 'false'/>
+      <Contact 
+      name = 'Joan Burton'
+      avatar = "https://randomuser.me/api/portraits/women/20.jpg"
+      Online = 'false'/>
+      <Contact 
+      name = 'Lori Banks'
+      avatar = "https://randomuser.me/api/portraits/women/3.jpg"
+      online = 'false'/>
     </div>
   );
 }
