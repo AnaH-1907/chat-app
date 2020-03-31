2fca0d374878fb325552bb51d3e4e7ac991ed9a7
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-31 12:40:54 +0200

    add boolean

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Contact from './components/Contact';
 function App () {
   return (
     <div className='App'>
-      <Contact name='Bonnie Day' avatar='https://randomuser.me/api/portraits/women/21.jpg' online={true} />
-      <Contact name='Joan Burton' avatar='https://randomuser.me/api/portraits/women/20.jpg' online={false} />
-      <Contact name='Lori Banks' avatar='https://randomuser.me/api/portraits/women/3.jpg' online={true} />
+      <Contact name='Bonnie Day' avatar='https://randomuser.me/api/portraits/women/21.jpg' alt='avatar Bonnie Day' online={true} />
+      <Contact name='Joan Burton' avatar='https://randomuser.me/api/portraits/women/20.jpg' alt='avatar Joan Burton' online={false} />
+      <Contact name='Lori Banks' avatar='https://randomuser.me/api/portraits/women/3.jpg' alt='avatar Lori Banks' online={true} />
     </div>
   );
 }
