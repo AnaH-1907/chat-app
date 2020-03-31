fe1881fd574c831a6b6147ed8d1f68266f6852af
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-31 12:47:48 +0200

    bool change

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Contact from './components/ContactList';
 function App () {
   return (
     <div className='App'>
-      <Contact name='Bonnie Day' avatar='https://randomuser.me/api/portraits/women/21.jpg' online='true' />
-      <Contact name='Joan Burton' avatar='https://randomuser.me/api/portraits/women/20.jpg' online='true' />
-      <Contact name='Lori Banks' avatar='https://randomuser.me/api/portraits/women/3.jpg' online='true' />
+      <Contact name='Bonnie Day' avatar='https://randomuser.me/api/portraits/women/21.jpg' online={true} />
+      <Contact name='Joan Burton' avatar='https://randomuser.me/api/portraits/women/20.jpg' online={false} />
+      <Contact name='Lori Banks' avatar='https://randomuser.me/api/portraits/women/3.jpg' online={true} />
     </div>
   );
 }
