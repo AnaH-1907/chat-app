a4ae52c00836c33602c3da6862f452e474dfa12d
Anaïs Huoch <anaisgagneur@gmail.com>
2020-03-31 12:05:33 +0200

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import Contact from './components/Contact';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <Contact 
-      name = 'Bonnie Day'
-      avatar = "https://randomuser.me/api/portraits/women/21.jpg"
-      online = 'true'/>
-      <Contact 
-      name = 'Joan Burton'
-      avatar = "https://randomuser.me/api/portraits/women/20.jpg"
-      online = 'true'/>
-      <Contact 
-      name = 'Lori Banks'
-      avatar = "https://randomuser.me/api/portraits/women/3.jpg"
-      online = 'true'/>
+    <div className='App'>
+      <Contact name='Bonnie Day' avatar='https://randomuser.me/api/portraits/women/21.jpg' online='true' />
+      <Contact name='Joan Burton' avatar='https://randomuser.me/api/portraits/women/20.jpg' online='true' />
+      <Contact name='Lori Banks' avatar='https://randomuser.me/api/portraits/women/3.jpg' online='true' />
     </div>
   );
 }
